abcea7444d4860c051ed563079ed618e7e316a86
Ben Heasly <benjamin.heasly@gmail.com>
2025-10-03 12:14:08 -0400

    Data upload script use experimenter path part, also search for openephys files.

--- scripts/upload_data.py
+++ scripts/upload_data.py
@@ -29,35 +29,42 @@ def walk_flat(
     return flat_list
 
 
-def apply_session(
+def apply_placeholders(
     original: str,
+    experimenter: str,
     subject_id: str,
     session_date: date
 ) -> str:
-    """Fill in session placeholders <SUBJECT>, <MMDDYY>, and <MMDDYYYY>."""
-    session_mmddyyyy = session_date.strftime("%m%d%Y")
-    session_mmddyy = session_date.strftime("%m%d%y")
-    return original.replace("<SUBJECT>", subject_id).replace("<MMDDYY>", session_mmddyy).replace("<MMDDYYYY>", session_mmddyyyy)
+    """Fill in session placeholders <EXPERIMENTER> <SUBJECT>, <YY>, <YYYY>, <MM>, and <DD>."""
+    yy = session_date.strftime("%y")
+    yyyy = session_date.strftime("%Y")
+    mm = session_date.strftime("%m")
+    dd = session_date.strftime("%d")
+    applied = original.replace('<EXPERIMENTER>', experimenter).replace("<SUBJECT>", subject_id).replace("<YYYY>", yyyy).replace("<YY>", yy).replace("<MM>", mm).replace("<DD>", dd)
+    return applied
 
 
 def run_main(
     behavior_path: Path,
     behavior_txt_pattern: str,
     behavior_mat_pattern: str,
-    spikeglx_path: Path,
-    spikeglx_nidq_pattern: str,
+    ephys_path: Path,
+    spikeglx_meta_pattern: str,
+    openephys_oebin_pattern: str,
     remote_host: str,
-    data_path: Path,
+    raw_data_path: Path,
+    experimenter: str,
     subject: str,
     session_date: str,
+    qualifier: str,
     username: str,
     password: str,
-    qualifier: str
 ):
     # Resolve session-specific placeholders in glob patterns.
-    behavior_txt_pattern = apply_session(behavior_txt_pattern, subject, session_date)
-    behavior_mat_pattern = apply_session(behavior_mat_pattern, subject, session_date)
-    spikeglx_nidq_pattern = apply_session(spikeglx_nidq_pattern, subject, session_date)
+    behavior_txt_pattern = apply_placeholders(behavior_txt_pattern, experimenter, subject, session_date)
+    behavior_mat_pattern = apply_placeholders(behavior_mat_pattern, experimenter, subject, session_date)
+    spikeglx_meta_pattern = apply_placeholders(spikeglx_meta_pattern, experimenter, subject, session_date)
+    openephys_oebin_pattern = apply_placeholders(openephys_oebin_pattern, experimenter, subject, session_date)
 
     # Collect files to upload as a list of (source_root, source_relative, destination_relative)
     to_upload = []
@@ -68,25 +75,46 @@ def run_main(
     for txt_match in behavior_path.glob(behavior_txt_pattern):
         txt_relative = txt_match.relative_to(behavior_path)
         logging.info(f"  {txt_relative}")
-        destination_relative = Path(subject, session_mmddyyyy, "behavior", txt_match.name)
+        destination_relative = Path(experimenter, subject, session_mmddyyyy, "behavior", txt_match.name)
         to_upload.append((behavior_path, txt_relative, destination_relative))
 
     logging.info(f"Searching local behavior_root for .mat like: {behavior_mat_pattern}")
     for mat_match in behavior_path.glob(behavior_mat_pattern):
         mat_relative = mat_match.relative_to(behavior_path)
         logging.info(f"  {mat_relative}")
-        destination_relative = Path(subject, session_mmddyyyy, "behavior", mat_match.name)
+        destination_relative = Path(experimenter, subject, session_mmddyyyy, "behavior", mat_match.name)
         to_upload.append((behavior_path, mat_relative, destination_relative))
 
-    # Locate spikeglx nidq.meta files as representatives of overall run dirs.
-    logging.info(f"Searching local spikeglx_root for nidq.meta like: {spikeglx_nidq_pattern}")
-    for nidq_match in spikeglx_path.glob(spikeglx_nidq_pattern):
-        run_dir = nidq_match.parent
+    # Locate spikeglx meta files as representatives spikeglx run dirs.
+    logging.info(f"Searching local ephys_root for .meta like: {spikeglx_meta_pattern}")
+    spikeglx_metas = list(ephys_path.glob(spikeglx_meta_pattern))
+    logging.info(f"Found .meta matches: {spikeglx_metas}")
+    if spikeglx_metas:
+        # Take the shortest match -- eg the nidq.meta, not a probe/ap.meta.
+        spikeglx_meta = min(spikeglx_metas, key=lambda meta: len(meta.parts))
+        run_dir = spikeglx_meta.parent
+        logging.info(f"Found spikeglx run dir: {run_dir}")
         for spikglx_file in walk_flat(run_dir):
-            spikglx_relative = spikglx_file.relative_to(spikeglx_path)
+            spikglx_relative = spikglx_file.relative_to(ephys_path)
             logging.info(f"  {spikglx_relative}")
-            destination_relative = Path(subject, session_mmddyyyy, "ecephys", spikglx_file.relative_to(run_dir.parent))
-            to_upload.append((spikeglx_path, spikglx_relative, destination_relative))
+            destination_relative = Path(experimenter, subject, session_mmddyyyy, "ecephys", spikglx_file.relative_to(run_dir.parent))
+            to_upload.append((ephys_path, spikglx_relative, destination_relative))
+
+    # Locate openephys meta files as representatives recording dirs.
+    logging.info(f"Searching local ephys_root for .oebin like: {openephys_oebin_pattern}")
+    oebins = list(ephys_path.glob(openephys_oebin_pattern))
+    logging.info(f"Found .oebin matches: {oebins}")
+    if oebins:
+        # Walk up several parents from an .oebin to find the recording dir.
+        #   date/record_node/experiment/recording/structure.oebin
+        oebin = oebins[0]
+        run_dir = oebin.parent.parent.parent.parent
+        logging.info(f"Found openephys run dir: {run_dir}")
+        for openephys_file in walk_flat(run_dir):
+            openephys_relative = openephys_file.relative_to(ephys_path)
+            logging.info(f"  {openephys_relative}")
+            destination_relative = Path(experimenter, subject, session_mmddyyyy, "ecephys", openephys_relative.relative_to(run_dir.parent))
+            to_upload.append((ephys_path, openephys_relative, destination_relative))
 
     if qualifier:
         logging.info(f"Keeping only files that match qualifier: {qualifier}")
@@ -114,10 +142,10 @@ def run_main(
             # Call to open() will log connection attempts, results.
             c.open()
 
-            logging.info(f"Uploading to {data_path}:")
+            logging.info(f"Uploading to {raw_data_path}:")
             for source_root, source_relative, destination_relative in to_upload:
                 source = Path(source_root, source_relative)
-                destination = Path(data_path, destination_relative)
+                destination = Path(raw_data_path, destination_relative)
                 logging.info(f"  {destination_relative}")
                 c.run(f"mkdir -p {destination.parent.as_posix()}")
                 c.put(source.as_posix(), destination.as_posix())
@@ -142,26 +170,32 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument(
         "--behavior-txt-pattern", "-T",
         type=str,
-        help="Glob pattern to match behavior .txt files within BEHAVIOR_ROOT. May include placeholders <SUBJECT>, <MMDDYY> and <MMDDYYYY> (default: %(default)s)",
-        default="<SUBJECT>/**/*_<MMDDYY>_*.txt"
+        help="Glob pattern to match behavior .txt files within BEHAVIOR_ROOT. May include placeholders <EXPERIMENTER>, <SUBJECT>, <YYYY>, <YY>, <MM>, <DD> (default: %(default)s)",
+        default="<SUBJECT>/**/*_<MM><DD><YY>_*.txt"
     )
     parser.add_argument(
         "--behavior-mat-pattern", "-M",
         type=str,
-        help="Glob pattern to match behavior .mat files within BEHAVIOR_ROOT. May include placeholders <SUBJECT>, <MMDDYY> and <MMDDYYYY> (default: %(default)s)",
-        default="<SUBJECT>/**/*_<MMDDYY>_*.mat"
+        help="Glob pattern to match behavior .mat files within BEHAVIOR_ROOT. May include placeholders <EXPERIMENTER>, <SUBJECT>, <YYYY>, <YY>, <MM>, <DD> (default: %(default)s)",
+        default="<SUBJECT>/**/*_<MM><DD><YY>_*.mat"
     )
     parser.add_argument(
-        "--spikeglx-root", "-g",
+        "--ephys-root", "-E",
         type=str,
         help="Local root directory to search for a SpikeGLX run directory. (default: %(default)s)",
         default="/mnt/c/Users/labuser/Desktop/Data"
     )
     parser.add_argument(
-        "--spikeglx-nidq-pattern", "-G",
+        "--spikeglx-meta-pattern", "-S",
         type=str,
-        help="Glob pattern to match SpikeGLX nidq .meta files within SPIKEGLX_ROOT. May include placeholders <SUBJECT>, <MMDDYY> and <MMDDYYYY> (default: %(default)s)",
-        default="<SUBJECT>/**/*_<MMDDYYYY>_*.nidq.meta"
+        help="Glob pattern to match SpikeGLX .meta files within EPHYS_ROOT. May include placeholders <EXPERIMENTER>, <SUBJECT>, <YYYY>, <YY>, <MM>, <DD> (default: %(default)s)",
+        default="<SUBJECT>/**/*_<MM><DD><YYYY>_*.meta"
+    )
+    parser.add_argument(
+        "--openephys-oebin-pattern", "-O",
+        type=str,
+        help="Glob pattern to match OpenEphys structure.oebin files within EPHYS_ROOT. May include placeholders <EXPERIMENTER>, <SUBJECT>, <YYYY>, <YY>, <MM>, <DD> (default: %(default)s)",
+        default="<SUBJECT>/<YYYY>-<MM>-<DD>_*/**/structure.oebin"
     )
     parser.add_argument(
         "--remote-host", "-r",
@@ -176,10 +210,16 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         default=None
     )
     parser.add_argument(
-        "--data-root", "-a",
+        "--raw-data-root", "-a",
         type=str,
-        help="Remote root directory containing lab data. (default: %(default)s)",
-        default="/vol/cortex/cd4/geffenlab/data/"
+        help="Remote root directory containing lab raw data. (default: %(default)s)",
+        default="/vol/cortex/cd4/geffenlab/raw_data/"
+    )
+    parser.add_argument(
+        "--experimenter", "-e",
+        type=str,
+        help="Experimenter initials to group related data. (default: prompt for input)",
+        default=None
     )
     parser.add_argument(
         "--subject", "-s",
@@ -212,17 +252,25 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     behavior_mat_pattern = cli_args.behavior_mat_pattern
     logging.info(f"Using behavior .mat pattern: {behavior_mat_pattern}")
 
-    spikeglx_path = Path(cli_args.spikeglx_root)
-    logging.info(f"Uploading SpikeGLX files from : {spikeglx_path}")
+    ephys_path = Path(cli_args.ephys_root)
+    logging.info(f"Uploading ephys files from : {ephys_path}")
 
-    spikeglx_nidq_pattern = cli_args.spikeglx_nidq_pattern
-    logging.info(f"Using SpikeGLX nidq pattern: {spikeglx_nidq_pattern}")
+    spikeglx_meta_pattern = cli_args.spikeglx_meta_pattern
+    logging.info(f"Using SpikeGLX .meta pattern: {spikeglx_meta_pattern}")
+
+    openephys_oebin_pattern = cli_args.openephys_oebin_pattern
+    logging.info(f"Using Open Ephys .oebin pattern: {openephys_oebin_pattern}")
 
     remote_host = cli_args.remote_host
     logging.info(f"Uploading files to remote host: {remote_host}")
 
-    data_path = Path(cli_args.data_root)
-    logging.info(f"Uploading files to remote data root: {data_path}")
+    raw_data_path = Path(cli_args.raw_data_root)
+    logging.info(f"Uploading files to remote raw data root: {raw_data_path}")
+
+    experimenter = cli_args.experimenter
+    if experimenter is None:
+        experimenter = input("Experimenter initials: ").strip()
+    logging.info(f"Uploading files for experimenter: {experimenter}")
 
     subject = cli_args.subject
     if subject is None:
@@ -237,7 +285,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     qualifier = cli_args.qualifier
     if qualifier is None:
-        qualifier = input("Qualifier like 'training', or 'ap.bin'.  Leave blank to upload all: ").strip()
+        qualifier = input("Qualifier like 'training','ap.bin', 'recording1', etc.  Leave blank to upload all: ").strip()
     if qualifier:
         logging.info(f"Uploading files matching qualifier: {qualifier}.")
     else:
@@ -256,15 +304,17 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
             behavior_path,
             behavior_txt_pattern,
             behavior_mat_pattern,
-            spikeglx_path,
-            spikeglx_nidq_pattern,
+            ephys_path,
+            spikeglx_meta_pattern,
+            openephys_oebin_pattern,
             remote_host,
-            data_path,
+            raw_data_path,
+            experimenter,
             subject,
             session_date,
+            qualifier,
             username,
             password,
-            qualifier
         )
     except:
         logging.error("Error uploading files.", exc_info=True)
