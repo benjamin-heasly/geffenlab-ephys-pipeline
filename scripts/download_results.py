0f4d418d2a4f9d57a9946f42d5f6a2a5b1366dbb
Ben Heasly <benjamin.heasly@gmail.com>
2025-11-24 16:10:50 -0500

    download_results.py include analysys/ and processed_data/ path parts.

--- scripts/download_results.py
+++ scripts/download_results.py
@@ -58,7 +58,7 @@ def run_main(
             analysis_files = analysis_result.stdout.strip().split('\n')
             for analysis_file in analysis_files:
                 relative_file_path = Path(analysis_file).relative_to(remote_analysis_path)
-                local_file_path = Path(local_path, experimenter, subject_id, date_string, relative_file_path)
+                local_file_path = Path(local_path, "analysis", experimenter, subject_id, date_string, relative_file_path)
                 c.get(remote=analysis_file, local=local_file_path.as_posix())
 
         except Exception:
@@ -88,7 +88,7 @@ def run_main(
                 logging.info(f"Downloading files to: {local_path}")
                 for remote_file in remote_files:
                     relative_file_path = Path(remote_file).relative_to(remote_processed_data_path)
-                    local_file_path = Path(local_path, experimenter, subject_id, date_string, relative_file_path)
+                    local_file_path = Path(local_path,"processed_data", processed_subdir, experimenter, subject_id, date_string, relative_file_path)
                     c.get(remote=remote_file, local=local_file_path.as_posix())
 
         except Exception:
